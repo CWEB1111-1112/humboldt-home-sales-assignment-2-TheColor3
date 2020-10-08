91226ddba9de6befd06b102cb5f8b9a16501115b
Crain <crajacr@dunwoody.edu>
2020-10-08 12:58:35 -0500

    Added Program.cs and 1112Assignment2.csproj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1112_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
